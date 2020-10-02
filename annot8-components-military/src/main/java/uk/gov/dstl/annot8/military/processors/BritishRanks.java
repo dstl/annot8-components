36fbe257c2c4a2498e1dbddfef610c59c7970353
James Baker <jdbaker@dstl.gov.uk>
2020-10-02 07:35:28 +0100

    Extract British Ranks

---
+++ annot8-components-military/src/main/java/uk/gov/dstl/annot8/military/processors/BritishRanks.java
@@ -0,0 +1,66 @@
+package uk.gov.dstl.annot8.military.processors;
+
+import io.annot8.api.annotations.Annotation;
+import io.annot8.api.capabilities.Capabilities;
+import io.annot8.api.components.annotations.ComponentDescription;
+import io.annot8.api.components.annotations.ComponentName;
+import io.annot8.api.components.annotations.ComponentTags;
+import io.annot8.api.context.Context;
+import io.annot8.api.settings.NoSettings;
+import io.annot8.common.components.AbstractProcessorDescriptor;
+import io.annot8.common.components.capabilities.SimpleCapabilities;
+import io.annot8.common.data.bounds.SpanBounds;
+import io.annot8.common.data.content.Text;
+import io.annot8.components.base.text.processors.AbstractRegexProcessor;
+import io.annot8.conventions.AnnotationTypes;
+import io.annot8.conventions.PropertyKeys;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@ComponentName("British Ranks")
+@ComponentDescription("Extracts British military rank abbreviations from text")
+@ComponentTags({"military", "ranks"})
+public class BritishRanks
+  extends AbstractProcessorDescriptor<BritishRanks.Processor, NoSettings> {
+
+  @Override
+  protected Processor createComponent(Context context, NoSettings noSettings) {
+    return new Processor();
+  }
+
+  @Override
+  public Capabilities capabilities() {
+    return new SimpleCapabilities.Builder()
+      .withCreatesAnnotations(AnnotationTypes.ANNOTATION_TYPE_PERSON, SpanBounds.class)
+      .withProcessesContent(Text.class)
+      .build();
+  }
+
+  public static class Processor extends AbstractRegexProcessor {
+    public Processor() {
+      super(
+        Pattern.compile("\\b(?<rank>(Flt Lt|Lt Gen|Lt Col|Air Cdre|Gp Capt|Wg Cdr|Sqn Sgt Maj|" +
+          "Adm|VAdm|RAdm|Cdre|Lt Cdr|Cdr|Sub Lt|Lt|SLt|Mid|OC|WO1|WO2|CPO|PO|AB|" +
+          "FM|Gen|Maj Gen|Brig|Col|Maj|Capt|2Lt|OCdt|SSgt|CSgt|Sgt|Cpl|LCpl|Pte|" +
+          "MRAF|Air Chf Mshl|Air Mshl|AVM|Sqn Ldr|Fg Off|Plt Off|Off Cdt|WO|FS|Chf Tech|SAC Tech|SAC ?\\(T\\)|SAC|LAC|MAcr" +
+          "Kgn|Rfn|LSgt|Cfn|Gdmn)|" +
+          "((Flight |Sub |Second )?Lieutenant|Lieutenant( General| Colonel)?|(Air )?Commodore|(Group )?Captain|Wing Commander|(Squadron Sergeant )?Major|" +
+          "(Vice |Rear )?Admiral|(Lieutenant )?Commander|Midshipman|Officer Cadet|Warrant Officer( [12])?|(Chief )?Petty Officer|(Leading|Able) Rating)|" +
+          "RAF Master Aircrew|RAF Flight Sergeant Aircrew|RAF Sergeant Aircrew" +
+          "Field Marshal|(Major )?General|Brigadier|Colonel|(Staff | Colour |Color |Flight |Lance )?Sergeant|(Lance )?Corporal|Private|" +
+          "Marshal of the Royal Air Force|Air (Chief |Vice-)?Marshal|Squadron Leader|(Flying|Pilot) Officer|Chief Technician|Senior Aircraftman \\(?Tech(nician)?\\)?|(Senior |Leading )?Aircraftman|" +
+          "Kingsman|Rifleman|Craftsman|Guardsman)" +
+          "(?<name>( [A-Z][-'A-Za-z]*)+)\\b"),
+        0,
+        AnnotationTypes.ANNOTATION_TYPE_PERSON);
+    }
+
+    @Override
+    protected void addProperties(Annotation.Builder builder, Matcher m) {
+      builder
+        .withProperty("rank", m.group("rank"))
+        .withProperty(PropertyKeys.PROPERTY_KEY_NAME, m.group("name").trim());
+    }
+  }
+}
---
+++ annot8-components-military/src/test/java/uk/gov/dstl/annot8/military/processors/BritishRanksTest.java
@@ -0,0 +1,99 @@
+/*
+ * Crown Copyright (C) 2019 Dstl
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.gov.dstl.annot8.military.processors;
+
+import io.annot8.api.annotations.Annotation;
+import io.annot8.api.properties.ImmutableProperties;
+import io.annot8.api.settings.NoSettings;
+import io.annot8.api.stores.AnnotationStore;
+import io.annot8.conventions.AnnotationTypes;
+import io.annot8.conventions.PropertyKeys;
+import io.annot8.testing.testimpl.TestItem;
+import io.annot8.testing.testimpl.content.TestStringContent;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.AnnotatedType;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class BritishRanksTest {
+
+  @Test
+  public void testCreation() {
+    BritishRanks br = new BritishRanks();
+    BritishRanks.Processor brProc = br.createComponent(null, NoSettings.getInstance());
+    assertNotNull(brProc);
+    brProc.close();
+  }
+
+  @Test
+  public void testDetection() {
+    test("Last week, Pte SMITH joined the Army", "Pte", "SMITH");
+    test("Last week, Private SMITH joined the Army", "Private", "SMITH");
+    test("Sqn Sgt Maj J Davies", "Sqn Sgt Maj", "J Davies");
+    test("Squadron Sergeant Major J Davies", "Squadron Sergeant Major", "J Davies");
+    test("Gp Capt HELEN JONES", "Gp Capt", "HELEN JONES");
+    test("Group Captain HELEN JONES", "Group Captain", "HELEN JONES");
+    test("On Thursday it was announced that 2Lt Baker would get a promotion.", "2Lt", "Baker");
+    test("On Thursday it was announced that Second Lieutenant Baker would get a promotion.", "Second Lieutenant", "Baker");
+    test("Off Cdt O'Neill", "Off Cdt", "O'Neill");
+    test("Officer Cadet O'Neill", "Officer Cadet", "O'Neill");
+    test("Col Sam Jones-Smith", "Col", "Sam Jones-Smith");
+    test("Colonel Sam Jones-Smith", "Colonel", "Sam Jones-Smith");
+  }
+
+  @Test
+  public void testMultipleDetection() {
+    TestItem testItem = new TestItem();
+    TestStringContent content =
+      testItem
+        .createContent(TestStringContent.class)
+        .withData("Capt Jones and CSgt Doe were last seen on Monday")
+        .save();
+
+    BritishRanks br = new BritishRanks();
+    BritishRanks.Processor brProc = br.createComponent(null, NoSettings.getInstance());
+    brProc.process(testItem);
+
+    assertEquals(2L, content.getAnnotations().getAll().count());
+    assertEquals(2L, content.getAnnotations().getByType(AnnotationTypes.ANNOTATION_TYPE_PERSON).count());
+  }
+
+  private void test(String text, String rank, String name){
+    TestItem testItem = new TestItem();
+    TestStringContent content =
+      testItem
+        .createContent(TestStringContent.class)
+        .withData(text)
+        .save();
+
+    BritishRanks br = new BritishRanks();
+    BritishRanks.Processor brProc = br.createComponent(null, NoSettings.getInstance());
+    brProc.process(testItem);
+
+    assertEquals(1L, content.getAnnotations().getAll().count());
+    assertEquals(1L, content.getAnnotations().getByType(AnnotationTypes.ANNOTATION_TYPE_PERSON).count());
+
+    Annotation a = content.getAnnotations().getByType(AnnotationTypes.ANNOTATION_TYPE_PERSON).findFirst().get();
+
+    assertEquals(rank + " " + name, a.getBounds().getData(content).orElse("** BAD **"));
+    assertEquals(rank, a.getProperties().get("rank").orElse("** BAD **"));
+    assertEquals(name, a.getProperties().get(PropertyKeys.PROPERTY_KEY_NAME).orElse("** BAD **"));
+  }
+}
